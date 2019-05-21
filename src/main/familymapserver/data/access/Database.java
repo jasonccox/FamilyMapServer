d860e8a3ef8dbf1b7de6d4378fd6f164da418973
Jason Cox <dev@jasoncarloscox.com>
2019-05-20 19:11:35 -0600

    implemented Database class with tests

--- src/main/familymapserver/data/access/Database.java
+++ src/main/familymapserver/data/access/Database.java
@@ -1,6 +1,8 @@
 package familymapserver.data.access;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Stores a connection to the database. By wrapping this connection in a class,
@@ -9,24 +11,57 @@ import java.sql.Connection;
  */
 public class Database {
     
+    /**
+     * The path to the database, relative to the project's top directory
+     */
+    private static final String DB_PATH = "db/db.sqlite";
+
+    private static final String DRIVER = "org.sqlite.JDBC";
+
     private Connection connection;
 
     /**
-     * Creates a new Database object. If the database does not already exist on disk,
-     * a new one will be created.
+     * Creates a new Database object. 
      */
-    public Database() {
-
+    public Database() throws DBException {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new DBException("Database driver could not be found.\n" + e.getMessage());
+        } 
     }
 
     /**
-     * Opens the connection to the database.
+     * Opens the connection to the database. If the database does not already exist on disk,
+     * a new one will be created.
      * 
      * @throws DBException if the database is already open, or if another database error 
      * occurs
      */
     public void open() throws DBException {
+        open(DB_PATH);
+    }
 
+     /**
+     * Opens the connection to the database. If the database does not already exist on disk,
+     * a new one will be created.
+     * 
+     * @param dbPath the path to the database, relative to the project's top directory
+     * @throws DBException if the database is already open, or if another database error 
+     * occurs
+     */
+    protected void open(String dbPath) throws DBException {
+        try {
+
+            if (connection != null) {
+                throw new DBException("Cannot open the database when it is already open.");
+            }
+
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+            connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            throw new DBException("Database connection could not be opened.\n" + e.getMessage());
+        }
     }
 
     /**
@@ -37,6 +72,14 @@ public class Database {
      */
     public void close() throws DBException {
 
+        this.rollback();
+
+        try {
+            connection.close();
+            connection = null;
+        } catch (SQLException e) {
+            throw new DBException("Database connection could not be closed.\n" + e.getMessage());
+        }
     }
 
     /**
@@ -46,7 +89,15 @@ public class Database {
      * occurs
      */
     public void commit() throws DBException {
+        if (connection == null) {
+            throw new DBException("Cannot commit to the database when it is closed.");
+        }
 
+        try {
+            connection.commit();
+        } catch (SQLException e) {
+            throw new DBException("Database transaction could not be committed.\n" + e.getMessage());
+        }
     }
 
     /**
@@ -56,7 +107,15 @@ public class Database {
      * occurs
      */
     public void rollback() throws DBException {
+        if (connection == null) {
+            throw new DBException("Cannot rollback the database when it is closed.");
+        }
 
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            throw new DBException("Database transaction could not be rolled back.\n" + e.getMessage());
+        }
     }
 
     /**
@@ -66,7 +125,14 @@ public class Database {
      * occurs
      */
     public void clear() throws DBException {
+        if (connection == null) {
+            throw new DBException("Cannot clear the database when it is closed.");
+        }
 
+        UserAccess.clear(this);
+        AuthTokenAccess.clear(this);
+        PersonAccess.clear(this);
+        EventAccess.clear(this);
     }
 
     /**
---
+++ src/test/familymapserver/data/access/DatabaseTest.java
@@ -0,0 +1,205 @@
+package familymapserver.data.access;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import familymapserver.data.model.AuthToken;
+import familymapserver.data.model.Event;
+import familymapserver.data.model.Person;
+import familymapserver.data.model.User;
+
+public class DatabaseTest {
+
+    private static final String TEST_DB = "db/test.sqlite";
+
+    private Database d;
+
+    @Before
+    public void setup() throws DBException {
+        File dbFile = new File(TEST_DB);
+        if (dbFile.exists()) {
+            dbFile.delete();
+        }
+
+        d = new Database();
+    }
+
+    @After
+    public void cleanup() throws DBException, SQLException {
+        if (d == null) {
+            return;
+        }
+
+        Connection c = d.getSQLConnection();
+
+        if (c != null && !c.isClosed()) {
+            d.close();
+        }
+
+        d = null;
+    }
+
+    @Test
+    public void constructorRunsNoException() throws DBException {
+        Database db = new Database();
+        assertNull(db.getSQLConnection());
+    }
+
+    @Test
+    public void openOpensConnection() throws DBException, SQLException {
+        d.open();
+        assertFalse(d.getSQLConnection().isClosed());
+    }
+
+    @Test (expected = DBException.class)
+    public void openThrowsExceptionIfOpen() throws DBException {
+        d.open();
+        d.open();
+    }
+
+    @Test
+    public void closeClosesConnection() throws DBException, SQLException {
+        d.open();
+        d.close();
+        assertNull(d.getSQLConnection());
+    }
+
+    @Test (expected = DBException.class)
+    public void closeThrowsExceptionIfClosed() throws DBException {
+        d.close();
+    }
+
+    @Test
+    public void closeRollsBackChanges() throws DBException, SQLException {
+        d.open(TEST_DB);
+        
+        Connection c = d.getSQLConnection();
+        PreparedStatement ps = c.prepareStatement("CREATE TABLE test (test_field INTEGER)");
+        ps.executeUpdate();
+
+        d.close();
+
+        d.open(TEST_DB);
+
+        c = d.getSQLConnection();
+        ps = c.prepareStatement("SELECT count(*) FROM sqlite_master WHERE type = 'table'");
+        ResultSet rs = ps.executeQuery();
+        assertEquals(0, rs.getInt(1));
+        rs.close();
+
+        d.close();
+    }
+
+    @Test
+    public void commitSavesChanges() throws DBException, SQLException {
+        d.open(TEST_DB);
+
+        Connection c = d.getSQLConnection();
+        PreparedStatement ps = c.prepareStatement("CREATE TABLE test (test_field INTEGER)");
+        ps.executeUpdate();
+
+        d.commit();
+        d.close();
+
+        d.open(TEST_DB);
+
+        c = d.getSQLConnection();
+        ps = c.prepareStatement("SELECT count(*) FROM sqlite_master WHERE type = 'table'");
+        ResultSet rs = ps.executeQuery();
+        assertEquals(1, rs.getInt(1));
+        rs.close();
+
+        d.close();
+    }
+
+    @Test (expected = DBException.class)
+    public void commitThrowsExceptionIfClosed() throws DBException {
+        d.commit();
+    }
+
+    @Test
+    public void rollbackRollsBackChanges() throws DBException, SQLException {
+        d.open(TEST_DB);
+
+        Connection c = d.getSQLConnection();
+        PreparedStatement ps = c.prepareStatement("CREATE TABLE test (test_field INTEGER)");
+        ps.executeUpdate();
+
+        d.rollback();
+        d.close();
+
+        d.open(TEST_DB);
+
+        c = d.getSQLConnection();
+        ps = c.prepareStatement("SELECT count(*) FROM sqlite_master WHERE type = 'table'");
+        ResultSet rs = ps.executeQuery();
+        assertEquals(0, rs.getInt(1));
+        rs.close();
+
+        d.close();
+    }
+
+    @Test (expected = DBException.class)
+    public void rollbackThrowsExceptionIfClosed() throws DBException {
+        d.rollback();
+    }
+
+    @Test
+    public void clearRemovesAllDataFromTables() throws DBException, SQLException {
+        d.open(TEST_DB);
+
+        UserAccess.createTable(d);
+        UserAccess.add(new User("test", "pw", "a", "b", "c", "d", "e"), d);
+
+        AuthTokenAccess.createTable(d);
+        AuthTokenAccess.add(new AuthToken("a", "b"), d);
+
+        PersonAccess.createTable(d);
+        PersonAccess.add(new Person("a", "b", "c", "d", "e", "f", "g", "h"), d);
+
+        EventAccess.createTable(d);
+        EventAccess.add(new Event("a", "b", "c", 0, 1, "d", "e", "f", 2), d);
+
+        d.clear();
+
+        Connection c = d.getSQLConnection();
+        PreparedStatement ps = c.prepareStatement("SELECT count() FROM user");
+        ResultSet rs = ps.executeQuery();
+        assertEquals(0, rs.getInt(1));
+        rs.close();
+
+        ps = c.prepareStatement("SELECT count() FROM auth_token");
+        rs = ps.executeQuery();
+        assertEquals(0, rs.getInt(1));
+        rs.close();
+
+        ps = c.prepareStatement("SELECT count() FROM person");
+        rs = ps.executeQuery();
+        assertEquals(0, rs.getInt(1));
+        rs.close();
+
+        ps = c.prepareStatement("SELECT count() FROM event");
+        rs = ps.executeQuery();
+        assertEquals(0, rs.getInt(1));
+        rs.close();   
+        
+        d.close();
+    }
+
+    @Test (expected = DBException.class)
+    public void clearThrowsExceptionIfClosed() throws DBException {
+        d.clear();
+    }
+
+}
